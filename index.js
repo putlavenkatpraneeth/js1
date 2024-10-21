7e01edaccf0d9409a257c701931da5100913d4f8
putla venkat praneeth <putlavenkat123@gmail.com>
2024-10-21 18:20:03 +0530

    changes

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log(x);
+var x=5;//undefined
+
+console.log(a);
+var a;//undefined
+
+console.log(b);
+b = 10;
+var b;//undefined
+
+console.log(c);//c is not defined
+
+console.log(e);//undefined
+var e = 10;
+console.log(e);//10
+e = 20;
+console.log(e);//20
+
+console.log(f);//undefined
+var f = 100;
+var f;
+console.log(f);//100
+
+console.log(g);//undefined
+var g = g + 1;
+console.log(g);//NaN
+
+console.log(i);//undefined
+i = 10;
+var i = 5;
+console.log(i);//5
+
+var h;
+console.log(h);//undefined
+h = 50;
+console.log(h);//50
